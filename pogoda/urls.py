aa70e9738a5dd1a42511106ddbe8f8c799061ec9
ZiN <wgalka2@op.pl>
2019-01-11 20:54:19 +0100

    basic layout v1

--- pogoda/urls.py
+++ pogoda/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='mapa'),
-    path('post/', views.post_list, name='post_list'),
+    path('', views.mapa, name='mapa'),
+    path('post', views.post_list, name='post_list'),
     path('index', views.index, name='index'),
+    path('pogoda', views.pogodamiasto, name='pogodamiasto'),
 ]
--- pogoda/views.py
+++ pogoda/views.py
@@ -5,6 +5,12 @@ from .models import Post
 def index(request):
     return render(request, 'pogoda\index.html')
 
+def mapa(request):
+    return render(request, 'pogoda\mapa.html')
+
+def pogodamiasto(request):
+    return render(request, 'pogoda\pogodamiasto.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'pogoda\post_list.html', {'posts': posts})
