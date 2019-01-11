e4f429363adf369ec81eecca86502b9b676924ba
ZiN <wgalka2@op.pl>
2019-01-11 20:45:14 +0100

    basic layout v1

--- pogoda/urls.py
+++ pogoda/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='mapa'),
+    path('post/', views.post_list, name='post_list'),
+    path('index', views.index, name='index'),
 ]
