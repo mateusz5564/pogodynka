d1a638c12dec3b75c7c321f1fd21ec3f100a6fff
ZiN <wgalka2@op.pl>
2019-01-11 22:21:15 +0100

    basic layout v1

--- pogoda/views.py
+++ pogoda/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+from yweather import yweather
+
 def index(request):
     return render(request, 'pogoda\index.html')
 
@@ -9,7 +11,15 @@ def mapa(request):
     return render(request, 'pogoda\mapa.html')
 
 def pogodamiasto(request):
-    return render(request, 'pogoda\pogodamiasto.html')
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location('dublin')
+    condition = location.condition
+    condition = condition.text
+
+    return render(request, 'pogoda\pogodamiasto.html', {'condition', condition})
+
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
