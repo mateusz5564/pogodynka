73178150d7636c47b5cfcd13cc6420c15b06e982
ZiN <wgalka2@op.pl>
2019-01-15 18:47:04 +0100

    ikony

--- pogoda/views.py
+++ pogoda/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
-from yweather import yweather
+import requests
 
 def index(request):
     return render(request, 'pogoda\index.html')
@@ -11,12 +10,11 @@ def mapa(request):
     return render(request, 'pogoda\mapa.html')
 
 def pogodamiasto(request):
-    weather = Weather(unit=Unit.CELSIUS)
-    location = weather.lookup_by_location('dublin')
-    condition = location.condition
-    condition = condition.text
+    condition = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=824a9043fdbd8f369532b5cb6320c78b')
 
-    return render(request, 'pogoda\pogodamiasto.html', {'condition', condition})
+    condition.s
+
+    return render(request, 'pogoda\pogodamiasto.html', {'condition' : condition})
 
 
 
