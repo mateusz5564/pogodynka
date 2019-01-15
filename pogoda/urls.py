41ef5580ed9cc80f3a11d96caaceadde1eafc230
mateusz5564 <mateusz5564@gmail.com>
2019-01-15 21:27:22 +0100

    Wyswietlanie danych z api

--- pogoda/urls.py
+++ pogoda/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mapa, name='mapa'),
+    path('sss', views.mapa, name='mapa'),
     path('post', views.post_list, name='post_list'),
     path('index', views.index, name='index'),
-    path('pogoda', views.pogodamiasto, name='pogodamiasto'),
+    path('', views.pogodamiasto, name='pogodamiasto'),
 ]
--- pogoda/views.py
+++ pogoda/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import Post
 import requests
 
+
+
 def index(request):
     return render(request, 'pogoda\index.html')
 
@@ -10,11 +12,26 @@ def mapa(request):
     return render(request, 'pogoda\mapa.html')
 
 def pogodamiasto(request):
-    condition = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=824a9043fdbd8f369532b5cb6320c78b')
 
-    condition.s
+    city = "Warsaw"
+    city_weather = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=Warsaw,pl&appid=824a9043fdbd8f369532b5cb6320c78b').json()
 
-    return render(request, 'pogoda\pogodamiasto.html', {'condition' : condition})
+    pogoda = []
+
+    for x in range(0, len(city_weather['list'])):
+        weather = {
+            'city' : city,
+            'temperature': city_weather['list'][x]['main']['temp'],
+            'description': city_weather['list'][x]['weather'][0]['description'],
+            'icon': city_weather['list'][x]['weather'][0]['icon'],
+            'dt_txt': city_weather['list'][x]['dt_txt'],
+        }
+        pogoda.append(weather)
+
+    print("SŁOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWNIK")
+    print("dlugosc " + str(len(city_weather['list'])))
+    print(pogoda)
+    return render(request, 'pogoda\pogodamiasto.html', {'pogoda' : pogoda})
 
 
 
