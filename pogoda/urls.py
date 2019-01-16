a11757b175eb8698fa62730dddacbe1a7bc08a41
ZiN <wgalka2@op.pl>
2019-01-16 11:11:03 +0100

    mapa leaflet plugin

--- pogoda/urls.py
+++ pogoda/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sss', views.mapa, name='mapa'),
+    path('mapa', views.mapa, name='mapa'),
     path('post', views.post_list, name='post_list'),
-    path('index', views.index, name='index'),
     path('', views.pogodamiasto, name='pogodamiasto'),
 ]
